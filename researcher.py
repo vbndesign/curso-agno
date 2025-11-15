90e01575d5ecdba2f9bb11d8b3c401462ddbf76c
Vinicius Bispo <vbndesign@gmail.com>
2025-11-15 02:08:48 -0300

    Projeto de agente que realiza pesquisa na internet através do Tavily

---
+++ researcher.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from agno.agent import Agent
+from agno.tools.tavily import TavilyTools
+from agno.models.groq import Groq
+
+load_dotenv()
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[TavilyTools()],
+    debug_mode=True
+)
+
+agent.print_response("Use suas ferramentas para pesquisar a temperatura de hoje em Vila Velha, ES.")
