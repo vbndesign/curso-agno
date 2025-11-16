92e44cab44b516dc1e75481e7a94b1f541ba0954
Vinicius Bispo <vbndesign@gmail.com>
2025-11-16 19:21:52 -0300

    Banco de dados SQlite para prsistência do histórico da conversa

--- 1.2-analista.py
+++ 1.2-analista.py
@@ -1,14 +1,25 @@
 from dotenv import load_dotenv
 from agno.agent import Agent
 from agno.tools.yfinance import YFinanceTools
-from agno.models.groq import Groq
+from agno.models.openai import OpenAIChat
+from agno.db.sqlite import SqliteDb
 
+import os
 load_dotenv()
 
+# Setup SQLite database
+db = SqliteDb(db_file="tmp/data.db")
+
 agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile"),
+    name="Analista Financeiro",
+    model=OpenAIChat(id="gpt-5-nano", api_key=os.getenv("OPENAI_API_KEY")),
     tools=[YFinanceTools()],
-    instructions="Use tabelas para mostrar a informação final. Não inclua nenhum outro texto"
+    instructions="Use tabelas para mostrar a informação final. Não inclua nenhum outro texto",
+    db=db,
+    add_history_to_context=True,
+    num_history_runs=3
 )
 
-agent.print_response("Qual a cotação atual da Apple e da Tesla?", stream=True)
+agent.print_response("Qual a cotação atual da Petrobras?", session_id="petrobras_session", user_id="analista_petrobras")
+agent.print_response("Qual a cotação atual da Vale?", session_id="vale_session", user_id="analista_vale")
+agent.print_response("Quais empresas já consultamos a contação?", session_id="petrobras_session", user_id="analista_empresas")
